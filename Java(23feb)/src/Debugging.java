26b1100398d9afbd5f4d7f9b2621e46f6bec280b
pschizo18 <pschizo18@gmail.com>
2018-02-24 20:28:40 +0530

    24 feb

---
+++ Java(23feb)/src/Debugging.java
@@ -0,0 +1,15 @@
+
+public class Debugging {
+	//design, coding, testing, debugging, deployment
+	
+	//1) bad variables values
+	//2) faulty logic
+	//3) unwarranted assumptions
+	//4) assumptions about problems
+	//5) being not systematic, critical
+	
+	//Approaches to take
+	//debug prints method begins, end.
+	//print variables values
+	//unit testing
+}
